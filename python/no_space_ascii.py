d33a81d99302018b3463740fc794b574ec7cdffb
PotatoKingTheVII <57728721+PotatoKingTheVII@users.noreply.github.com>
2022-07-05 14:37:10 +0100

    Add hex option

--- python/no_space_ascii.py
+++ python/no_space_ascii.py
@@ -1,7 +1,7 @@
 def valid_ascii(number, mode):
     """
     Retruns true if printable ascii range
-    Mode: 0=Dec, 1=Oct, 2=Bin
+    Mode: 0=Dec, 1=Oct, 2=Hex, 3+ = Bin
     """
 
     #For edge case at end with empty number. Sanity check
@@ -12,7 +12,9 @@ def valid_ascii(number, mode):
         int_num = int(number, 10)
     elif(mode == 1):
         int_num = int(number, 8)
-    elif(mode > 1):
+    elif(mode == 2):
+        int_num = int(number, 16)
+    elif(mode > 2):
         int_num = int(number, 2)
 
     #Range that is considered valid, can up to 65 if ct is only letters etc for better results
@@ -24,7 +26,7 @@ def valid_ascii(number, mode):
 def space_brute(ct, mode):
     """
     Returns decoded plaintext after parsing with spacing.
-    Mode: 0=Dec, 1=Oct, 2=Bin
+    Mode: 0=Dec, 1=Oct, 2=Hex, 3+ = Bin
     """
     
     ct_len = len(ct)
@@ -46,16 +48,31 @@ def space_brute(ct, mode):
             i+=1    #If we're here then there's no valid option, jump forward one to avoid infinite loop
 
 
+    #Hex Section
+    if(mode == 2):
+        tmp_chunk = ""
+        
+        while i < ct_len - 1:   #Work through the CT
+            for j in range(2,0,-1): #Change window size till valid chunk found
+                tmp_chunk = ct[i:i+j]
+                if(valid_ascii(tmp_chunk, mode)):
+                   spaced_array.append(tmp_chunk)
+                   i+=j-1   #-1 because the infinite loop addition happens regardless next
+                   break
+                
+            i+=1    #If we're here then there's no valid option, jump forward one to avoid infinite loop
+
+
     #Binary Section - Forward scrubbing
-    if(mode == 2 or mode == 3):
+    if(mode == 3 or mode == 4):
         tmp_chunk = ""
 
-        if(mode == 2):  #High bytes
+        if(mode == 3):  #High bytes
             start = 7
             end = 0
             step = -1
 
-        else:   #Low bytes (mode = 3)
+        else:   #Low bytes (mode = 4)
             start = 1
             end = 8
             step = 1
@@ -72,16 +89,16 @@ def space_brute(ct, mode):
 
 
     #Binary Section - Backwards scrubbing
-    if(mode >3):
+    if(mode >4):
         i = ct_len #So we start from the back
         tmp_chunk = ""
 
-        if(mode == 4):  #High bytes
+        if(mode == 5):  #High bytes
             start = 7
             end = 0
             step = -1
 
-        else:   #Low bytes (mode = 3)
+        else:   #Low bytes (mode = 6)
             start = 1
             end = 8
             step = 1
@@ -102,9 +119,11 @@ def space_brute(ct, mode):
         base = 10
     elif(mode == 1):
         base = 8
-    elif(mode > 1):
+    elif(mode == 2):
+        base = 16
+    elif(mode > 2):
         base = 2
-    if(mode > 3):   #I.e. we worked backwards so reverse the array
+    if(mode > 4):   #I.e. we worked backwards so reverse the array
         spaced_array = spaced_array[::-1]
 
     plaintext = ""
@@ -114,6 +133,6 @@ def space_brute(ct, mode):
     return plaintext
 
 
-#Modes: 0=Dec, 1=Oct, 2=Bin-forward-high, 3=Bin-forward-low, 4=Bin-backwards-high, 5=Bin-backwards-low
+#Modes: 0=Dec, 1=Oct, 2 = Hex, 3=Bin-forward-high, 4=Bin-forward-low, 5=Bin-backwards-high, 6=Bin-backwards-low
 ciphertext = "733210497118101321001051159911111810111410110032973211611411710812132114101109971141079798108101321121141111111023211110232116104105115321161041011111141011093211910410599104321161041051153210997114103105110321051153211611111132115109971081083211611132991111101169710511046"
 print(space_brute(ciphertext, 0))
